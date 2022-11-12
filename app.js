98417a694ad03d090b2b5bdb49b86187933e04ae
Abdullahi Ahmad <abunnazeer@gmail.com>
2022-11-12 06:29:06 -0800

    working on navigation

--- app.js
+++ app.js
@@ -9,6 +9,56 @@ const scrollContainer = document.querySelector('.cat');
 const btnLeft = document.querySelector('.cat-scroll-left');
 const btnRight = document.querySelector('.cat-scroll-right');
 
+const allLocaton = [
+  {
+    imagePath: 'assets/img/image.png',
+    locationName: 'Ponta Delgada Portugal',
+    rating: 4.59,
+    desc: '4,56a kilomiters aways',
+    Date: 'jul 21-21',
+    price: '$295',
+    days: 'night',
+  },
+  {
+    imagePath: [
+      'assets/img/image.png',
+      'assets/img/image.png',
+      'assets/img/image.png',
+    ],
+    locationName: 'Ponta Delgada Portugal',
+    rating: 4.59,
+    desc: '4,56a kilomiters aways',
+    Date: 'jul 21-21',
+    price: '$295',
+    days: 'night',
+  },
+  {
+    imagePath: [
+      'assets/img/image.png',
+      'assets/img/image.png',
+      'assets/img/image.png',
+    ],
+    locationName: 'Ponta Delgada Portugal',
+    rating: 4.59,
+    desc: '4,56a kilomiters aways',
+    Date: 'jul 21-21',
+    price: '$295',
+    days: 'night',
+  },
+  {
+    imagePath: [
+      'assets/img/image.png',
+      'assets/img/image.png',
+      'assets/img/image.png',
+    ],
+    locationName: 'Ponta Delgada Portugal',
+    rating: 4.59,
+    desc: '4,56a kilomiters aways',
+    Date: 'jul 21-21',
+    price: '$295',
+    days: 'night',
+  },
+];
 accountIcon.addEventListener('click', () => {
   console.log(accountToggle.classList.toggle('drop-hide'));
 });
